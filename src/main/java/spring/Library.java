77aa96260e9e3fd54f780cea17055f729a188f9f
shevchcross <shevchcross@gmail.com>
2018-07-19 16:40:39 +0300

    Создал два класса Library and Reader, Создал новый bean файл реализовал @Autowired. в классе Reader

---
+++ src/main/java/spring/Library.java
@@ -0,0 +1,52 @@
+package spring;
+
+/**
+ * Created by Olya on 19.07.2018.
+ */
+public class Library {
+    private int year;
+    private String autor;
+    private String kind;
+
+    public Library(int year, String autor, String kind) {
+        this.year = year;
+        this.autor = autor;
+        this.kind = kind;
+    }
+
+    public Library() {
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getKind() {
+        return kind;
+    }
+
+    public void setKind(String kind) {
+        this.kind = kind;
+    }
+
+    @Override
+    public String toString() {
+        return "Library{" +
+                "year=" + year +
+                ", autor='" + autor + '\'' +
+                ", kind='" + kind + '\'' +
+                '}';
+    }
+}
